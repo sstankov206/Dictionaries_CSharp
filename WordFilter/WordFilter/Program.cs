c1cdb5ae611e6b4cb6994d69a14eabb3054e3d7b
Stan S <stankovst@gmail.com>
2022-12-08 15:31:07 -0800

    adding WordFilter

---
+++ WordFilter/WordFilter/Program.cs
@@ -0,0 +1,36 @@
+﻿/* ▪ Read a string array
+ * ▪ Print only words, whose length is even
+ * 
+ * Examples:
+ * 
+ * INPUT:       kiwi orange banana apple
+ * OUTPUT:      kiwi
+ *              orange
+ *              banana
+ *
+ * solution by me, sstankov.
+ */
+
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        //reading input:
+        //Ex: kiwi orange banana
+        string[] input = Console.ReadLine()
+                        .Split()
+                        .Where(word => word.Length % 2 == 0) //checks for even length of each word
+                        .ToArray(); 
+
+        //iterating thorugh each word in the array:
+        foreach (string word in input)
+        Console.WriteLine(word); //output
+    } 
+    
+}
+
+
