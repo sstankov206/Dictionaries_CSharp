f56bece480463a2ac4ce511fbe378a5727442205
Stan S <stankovst@gmail.com>
2022-12-22 15:35:44 -0800

    adding Substring

---
+++ Substring/Substring/Program.cs
@@ -0,0 +1,35 @@
+﻿/* ▪ You are given a text and a remove word
+ * ▪ Remove all substrings that are equal to the remove word
+ * 
+ * Examples:
+ * 
+ * INPUT:       key
+ *              keytextkey
+ *              
+ * OUTPUT:      text
+ * 
+ * Solution by me, sstankov.
+ */
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        //reading input:
+        string word = Console.ReadLine(); //word to remove
+        string text = Console.ReadLine(); //string of text to remove word from
+
+        int index = text.IndexOf(word);
+        while (index >= 0)
+        {
+            //remove the  keyword ("word"),
+            //starting at index and deleting as many characters as the word's length:
+            text = text.Remove(index, word.Length);
+            index = text.IndexOf(word);
+        }
+        //printing output:
+        Console.WriteLine(text);
+    }
+}
+
